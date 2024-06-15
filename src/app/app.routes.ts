e168a406e8a8491d9a8b9a5f0d78f1dba57c767b
Gusito9 <jesusmartincarmona6@gmail.com>
2024-06-15 22:16:49 +0200

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 export const routes: Routes = [
   { path: 'pesadas', component: PesadasComponent },
   { path: 'objetivos', component: ObjetivosComponent },
-  { path: '', redirectTo: '/pesadas', pathMatch: 'full' },
+  { path: '', redirectTo: '/objetivos', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
