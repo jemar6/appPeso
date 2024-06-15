ee33b4156830e1c7928ad54e979f45b10e020508
Gusito9 <jesusmartincarmona6@gmail.com>
2024-06-15 19:08:19 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PesadasComponent } from './pages/pesadas/pesadas.component';
+import { ObjetivosComponent } from './pages/objetivos/objetivos.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pesadas', component: PesadasComponent },
+  { path: 'objetivos', component: ObjetivosComponent },
+  { path: '', redirectTo: '/pesadas', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/pages/pesadas/pesadas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pesadas',
+  standalone: true,
+  imports: [],
+  templateUrl: './pesadas.component.html',
+  styleUrl: './pesadas.component.css'
+})
+export class PesadasComponent {
+
+}
